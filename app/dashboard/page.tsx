442a8c264556d032f20c163195f46d2e6db0a7ad
usmanxs <usman007be@gmail.com>
2023-08-31 00:49:59 +0500

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-
-
-
-
-
-
-
-
-
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import { getdistribor, searchProduct, getSectors, getAreaBySector } from '@/actions/actions';
-import { Button } from '@/components/ui/button';
+import React, { useState, useEffect, ChangeEvent } from "react";
+import {
+  getdistribor,
+  searchProduct,
+  getSectors,
+  getAreaBySector,
+} from "@/actions/actions";
+import { Button } from "@/components/ui/button";
 
 interface Distributor {
   name: string;
@@ -30,28 +26,35 @@ interface Area {
   seccd: number;
 }
 
-   
-
 export default function Home() {
-  const [selectedDistributor, setSelectedDistributor] = useState<number | ''>('');
-  const [distributorOptions, setDistributorOptions] = useState<Distributor[]>([]);
+  const [selectedDistributor, setSelectedDistributor] = useState<number | "">(
+    ""
+  );
+  const [distributorOptions, setDistributorOptions] = useState<Distributor[]>(
+    []
+  );
   const [filteredSectors, setFilteredSectors] = useState<Sector[]>([]);
-  const [selectedSector, setSelectedSector] = useState<number | ''>('');
+  const [selectedSector, setSelectedSector] = useState<number | "">("");
   const [seccd, setSeccd] = useState<number | null>(null);
   const [areas, setAreas] = useState<Area[]>([]);
-  const [selectedArea, setSelectedArea] = useState<number | ''>('');
+  const [selectedArea, setSelectedArea] = useState<number | "">("");
 
   useEffect(() => {
     getdistribor().then((backdistributor: any) => {
-      const distributorData: Distributor[] = backdistributor.map((s: any) => ({ name: s.name, code: s.dist_code }));
+      const distributorData: Distributor[] = backdistributor.map((s: any) => ({
+        name: s.name,
+        code: s.dist_code,
+      }));
       setDistributorOptions(distributorData);
     });
   }, []);
 
   useEffect(() => {
-    if (selectedDistributor !== '') {
+    if (selectedDistributor !== "") {
       const selectedSectors = getSectors().then((backendSectors: any) => {
-        const filteredSectorData: Sector[] = backendSectors.filter((s: any) => s.dist_code === selectedDistributor);
+        const filteredSectorData: Sector[] = backendSectors.filter(
+          (s: any) => s.dist_code === selectedDistributor
+        );
         setFilteredSectors(filteredSectorData);
       });
     } else {
@@ -72,7 +75,9 @@ export default function Home() {
     setSelectedDistributor(selectedDistributorCode);
 
     const selectedSectors = getSectors().then((backendSectors: any) => {
-      const filteredSectorData: Sector[] = backendSectors.filter((s: any) => s.dist_code === selectedDistributorCode);
+      const filteredSectorData: Sector[] = backendSectors.filter(
+        (s: any) => s.dist_code === selectedDistributorCode
+      );
       setFilteredSectors(filteredSectorData);
     });
   };
@@ -80,11 +85,15 @@ export default function Home() {
   const handleSectorChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedSectorCode = parseInt(event.target.value, 10);
     setSelectedSector(selectedSectorCode);
-    setSelectedArea('');
+    setSelectedArea("");
 
     const selectedSectors = getSectors().then((backendSectors: any) => {
-      const filteredSectorData: Sector[] = backendSectors.filter((s: any) => s.dist_code === selectedDistributor);
-      const selectedSector = filteredSectorData.find((sector) => sector.seccd === selectedSectorCode);
+      const filteredSectorData: Sector[] = backendSectors.filter(
+        (s: any) => s.dist_code === selectedDistributor
+      );
+      const selectedSector = filteredSectorData.find(
+        (sector) => sector.seccd === selectedSectorCode
+      );
       if (selectedSector) {
         setSeccd(selectedSector.seccd);
       }
@@ -97,69 +106,68 @@ export default function Home() {
   return (
     <main className=" mt-44 m-8 flex justify-center  flex-col ">
       <div>
-
-      <div className="py-4 " >
-        <label htmlFor="distributor" className="text-gray-600 font-medium">
-          Select Distributor:
-        </label>
-        <select
-          id="distributor"
-          onChange={handleDistributorChange}
-          value={selectedDistributor}
-          className="p-2 border rounded w-full"
+        <div className="py-4 ">
+          <label htmlFor="distributor" className="text-gray-600 font-medium">
+            Select Distributor:
+          </label>
+          <select
+            id="distributor"
+            onChange={handleDistributorChange}
+            value={selectedDistributor}
+            className="p-2 border rounded w-full"
           >
-          <option value="">Select...</option>
-          {distributorOptions.map((dist) => (
-            <option key={dist.code} value={dist.code}>
-              {dist.name}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      {filteredSectors.length > 0 && (
-        <div className="py-4">
-          <label htmlFor="sector" className="text-gray-600 font-medium">
-            Select Sector:
-          </label>
-          <select
-            id="sector"
-            onChange={handleSectorChange}
-            value={selectedSector}
-            className="p-2 border rounded w-full"
-            >
             <option value="">Select...</option>
-            {filteredSectors.map((sector) => (
-              <option key={sector.seccd} value={sector.seccd}>
-                {sector.name}
+            {distributorOptions.map((dist) => (
+              <option key={dist.code} value={dist.code}>
+                {dist.name}
               </option>
             ))}
           </select>
         </div>
-      )}
 
-      {seccd && areas.length > 0 && (
-        <div className="py-4">
-          <label htmlFor="area" className="text-gray-600 font-medium">
-            Select Area:
-          </label>
-          <select
-            id="area"
-            onChange={handleAreaChange}
-            value={selectedArea}
-            className="p-2 border rounded w-full"
+        {filteredSectors.length > 0 && (
+          <div className="py-4">
+            <label htmlFor="sector" className="text-gray-600 font-medium">
+              Select Sector:
+            </label>
+            <select
+              id="sector"
+              onChange={handleSectorChange}
+              value={selectedSector}
+              className="p-2 border rounded w-full"
             >
-            <option value="">Select...</option>
-            {areas.map((area) => (
-              <option key={area.id} value={area.seccd}>
-                {area.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
+              <option value="">Select...</option>
+              {filteredSectors.map((sector) => (
+                <option key={sector.seccd} value={sector.seccd}>
+                  {sector.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
 
-    <Button  className='w-full mt-4'>Submit</Button>
+        {seccd && areas.length > 0 && (
+          <div className="py-4">
+            <label htmlFor="area" className="text-gray-600 font-medium">
+              Select Area:
+            </label>
+            <select
+              id="area"
+              onChange={handleAreaChange}
+              value={selectedArea}
+              className="p-2 border rounded w-full"
+            >
+              <option value="">Select...</option>
+              {areas.map((area) => (
+                <option key={area.id} value={area.seccd}>
+                  {area.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        <Button className="w-full mt-4">Submit</Button>
       </div>
     </main>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,9 +4,9 @@ import { LoginCard } from "@/components/login";
 import { SelectInput } from "@/components/SelectInput";
 import { v4 } from "uuid";
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { getUserbyDistributor } from '@/actions/actions';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { getUserbyDistributor } from "@/actions/actions";
 
 interface LoginPageProps {
   query: {
@@ -15,29 +15,34 @@ interface LoginPageProps {
 }
 
 export default function LoginPage({ query }: LoginPageProps) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState(false);
   const router = useRouter();
 
   const handleLogin = async () => {
     try {
       const result = await getUserbyDistributor();
-      
-      if ('message' in result) {
+
+      if ("message" in result) {
         setLoginError(true);
         console.error(result.message);
         return;
       }
-      
+
       const users = result as any[];
-      console.log(result  )
-      const matchingUsers = users.filter((u) => u.dist_code === parseInt(query.distCode));
-      
-      const user = matchingUsers.find((u) => u.username === username && u.password === password && u.active === 1);
-      
+      console.log(result);
+      const matchingUsers = users.filter(
+        (u) => u.dist_code === parseInt(query.distCode)
+      );
+
+      const user = matchingUsers.find(
+        (u) =>
+          u.username === username && u.password === password && u.active === 1
+      );
+
       if (user) {
-        router.push('/dashboard');
+        router.push("/dashboard");
       } else {
         setLoginError(true);
       }
@@ -46,13 +51,14 @@ export default function LoginPage({ query }: LoginPageProps) {
       setLoginError(true);
     }
   };
-  
+
   return (
     <main className="w-full h-full">
-      
       <div className="mt-44 ">
         <div className="flex justify-center flex-col">
-         <div className="text-center text-xl"><h1>Login</h1></div>
+          <div className="text-center text-xl">
+            <h1>Login</h1>
+          </div>
           <LoginCard
             username={username}
             setUsername={setUsername}
